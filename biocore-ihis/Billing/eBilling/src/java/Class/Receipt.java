ac3265a1af20154b8fdf3f59f93bcfc5dd49b18f
mh0411 <z.mike0411@gmail.com>
2017-03-13 01:23:05 +0800

    Billing - Standardizing interface

--- biocore-ihis/Billing/eBilling/src/java/Class/Receipt.java
+++ biocore-ihis/Billing/eBilling/src/java/Class/Receipt.java
@@ -176,15 +176,15 @@ public class Receipt extends HttpServlet {
             ArrayList<ArrayList<String>> userData = Conn.getData(sql_getHFC);
             String hfc = userData.get(0).get(0);
             
-            String sql_getHFAddr = 
-                    "SELECT hfc_name, address1, address2, address3 "
-                    + "FROM adm_health_facility "
-                    + "WHERE hfc_cd = '"+ hfc +"'";
-            ArrayList<ArrayList<String>> hfData = Conn.getData(sql_getHFAddr);
-            String hfName = hfData.get(0).get(0);
-            String hfAddr1 = hfData.get(0).get(1);
-            String hfAddr2 = hfData.get(0).get(2);
-            String hfAddr3 = hfData.get(0).get(3); 
+//            String sql_getHFAddr = 
+//                    "SELECT hfc_name, address1, address2, address3 "
+//                    + "FROM adm_health_facility "
+//                    + "WHERE hfc_cd = '"+ hfc +"'";
+//            ArrayList<ArrayList<String>> hfData = Conn.getData(sql_getHFAddr);
+//            String hfName = hfData.get(0).get(0);
+//            String hfAddr1 = hfData.get(0).get(1);
+//            String hfAddr2 = hfData.get(0).get(2);
+//            String hfAddr3 = hfData.get(0).get(3); 
             
 //            String imgPath = getServletContext().getRealPath("/assets/img/LogoJawiUTeM.png");
 //            Image logo = Image.getInstance(imgPath);
@@ -197,13 +197,13 @@ public class Receipt extends HttpServlet {
 //            cell1.setLeading(15f, 0.3f);
 //            header.addCell(cell1);
             
-            String addr = 
-                    " "+ hfName +", \n"
-                    + " "+ hfAddr1 +" \n"
-                    + " "+ hfAddr2 +", \n"
-                    + " "+ hfAddr3;
+//            String addr = 
+//                    " "+ hfName +", \n"
+//                    + " "+ hfAddr1 +" \n"
+//                    + " "+ hfAddr2 +", \n"
+//                    + " "+ hfAddr3;
             
-            PdfPCell cellAddress = new PdfPCell(new Phrase(addr, rectemja));
+            PdfPCell cellAddress = new PdfPCell(new Phrase(hfc, rectemja));
             cellAddress.setHorizontalAlignment(Element.ALIGN_LEFT);
             cellAddress.setBorder(Rectangle.NO_BORDER);
             cellAddress.setColspan(2);
