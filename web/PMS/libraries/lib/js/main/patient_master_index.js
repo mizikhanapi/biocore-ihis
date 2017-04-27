48da29b645a4526abb8cd63d579c97c1f4d0206d
DESKTOP-HTDHAB4\shay <zarann.sz@gmail.com>
2017-04-27 16:43:11 +0800

    datepicker year setting changed

--- web/PMS/libraries/lib/js/main/patient_master_index.js
+++ web/PMS/libraries/lib/js/main/patient_master_index.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
     $(function () {
 
-        $('#PMIbday').datepicker({dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true});
+        $('#PMIbday').datepicker({
+            dateFormat: 'yy-mm-dd',
+            changeMonth: true,
+            changeYear: true,
+            maxDate: 0,
+            yearRange: "-100:+nn"});
 
 
     });
