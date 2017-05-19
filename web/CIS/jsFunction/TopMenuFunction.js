b9503ecb9c6bc6a2fd4255ab90d081a5b7a46466
Tirmizi Khanapi <mizikhanapi@gmail.com>
2017-05-19 15:07:08 +0800

    Revert "s"
    This reverts commit d244a68ea7f55b41ed985223cb77d058a4e46752.

--- web/CIS/jsFunction/TopMenuFunction.js
+++ web/CIS/jsFunction/TopMenuFunction.js
@@ -47,12 +47,16 @@ $(document).ready(function (e) {
     var vtsCounter = 0;
     //------------------------------------------------------------ DISCHARGE BUTTON
     $('#dischargeBtn').click(function () {
-        
-        //$('#mCIS_Discharge_Summary').modal('toggle');
+        reloadStat = 0;
+//        getORCHFCDetail("04010102", "04010101");
+//        console.log(ORCObj);
+//       var orc =  getORC("transectionCode","orderNo","fillerOrderNo","orderStatus", "orderPriority", "orderDateTime", "episodeDate", "encounterDateTime", "enterBy", "verifiedBy", "OPD", "orderHFC", "orderDis", "orderSub","orderApp", "orderHFCAdd1",
+//        "orderHFCAdd2", "orderHFCAdd3", "orderHFCtown", "orderHFCDistrict", "orderHFCState", "orderHFCCountry","orderHFCPostcode", "orderHFCPhoneNo", "providerHFC", "providerDis","providerSub","providerApp", "providerAdd1", 
+//        "providerAdd2","providerAdd3", "providerTown");
+//        console.log(orc);
         var pmiNo = $('#pmiNumber').text();
-      // $('#divCIS_Discharge_Summary').html($('#tblCIS_Consultation_Table').html());
-      getSettingConsult(doctor_id);
-       reloadStat = 0;
+       
+        getSettingConsult(doctor_id);
     });
 
     //------------------------------------------------------------ ON HOLD BUTTON
