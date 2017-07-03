2f32bec7b3a8c0a680dfa34c48ad7cfbb01b4af5
DESKTOP-HTDHAB4\shay <zarann.sz@gmail.com>
2017-07-03 15:45:23 +0800

    extractor added ARP message

--- Extractor/New/EXTRACTOR_v2/src/lhr_tables/lhr_ARP.java
+++ Extractor/New/EXTRACTOR_v2/src/lhr_tables/lhr_ARP.java
@@ -18,20 +18,23 @@ package lhr_tables;
 import Bean.ARP;
 import Config_Pack.Config;
 import bean.ARP2;
+import bean.ORC2;
 import java.util.ArrayList;
 import java.util.Vector;
 import main.RMIConnector;
+
 /**
  *
  * @author Shay
  */
 public class lhr_ARP {
+
     RMIConnector rc = new RMIConnector();
     private String query_alg_lhr_arp;
     //private Vector<ALG2> alg2;
     private int rowsARP;
-    
-    public void M_ARP(Vector<ARP2> arp2, get_ehr_central_data t) {
+
+    public void M_ARP(Vector<ARP2> arp2, get_ehr_central_data t, Vector<ORC2> orc) {
 
         int total_fail_insert = 0; //total of failed insert
         //set default value to true. When insertion failed var will switch to false and patient will noy update to 3
@@ -41,54 +44,87 @@ public class lhr_ARP {
 
             rowsARP = arp2.size();
             System.out.println("number: " + rowsARP);
+            for (int orc_i = 0; orc_i < orc.size(); orc_i++) {
+                ArrayList<ArrayList<String>> orcs = orc.get(orc_i).getValue();
+                if (orcs.get(1).get(0).equals("T12203")) {
+                    ARP arpB = new ARP(); //declare outside so can display data outside bracket
+                    if (rowsARP > 0) {
 
-            ARP arpB = new ARP(); //declare outside so can display data outside bracket
-            if (rowsARP > 0) {
+                        ArrayList<ARP> arpBr = new ArrayList<ARP>();
+                        for (int n = 0; n < rowsARP; n++) {
 
-                ArrayList<ARP> arpBr = new ArrayList<ARP>();
-                for (int n = 0; n < rowsARP; n++) {
+                            try {
 
-                    try {
+                                ArrayList<ArrayList<String>> alArp = arp2.get(n).getValue();
 
-                        ArrayList<ArrayList<String>> alArp = arp2.get(n).getValue();
+                                arpB.setPMI_no(t.getPmi_no());
+                                arpB.setDateTime(alArp.get(2).get(0));
+                                arpB.setPrCode(alArp.get(1).get(0));
+                                arpB.setPrName(alArp.get(1).get(1));
+                                arpB.setPrCodeStd(alArp.get(1).get(2));
+                                arpB.setPrDuration(alArp.get(3).get(0));
+                                arpB.setDcgCode(alArp.get(4).get(0));
+                                arpB.setDocId(alArp.get(5).get(0));
+                                arpB.setDocName(alArp.get(6).get(0));
+                                arpB.setLocCode(alArp.get(7).get(0));
+                                arpB.setLocName(alArp.get(8).get(0));
+                                arpB.setComment(alArp.get(9).get(0));
+                                arpB.setRecHFCcode(alArp.get(10).get(0));
+                                arpB.setEpisodeDate(alArp.get(11).get(0));
+                                arpB.setEncounter_Date(orcs.get(8).get(0));
 
-                        arpB.setPMI_no(t.getPmi_no());
-                        arpB.setDateTime(alArp.get(2).get(0));
-                        arpB.setPrCode(alArp.get(1).get(0));
-                        arpB.setPrName(alArp.get(1).get(1));
-                        arpB.setPrCodeStd(alArp.get(1).get(2));
-                        arpB.setPrDuration(alArp.get(3).get(0));
-                        arpB.setDcgCode(alArp.get(4).get(0));
-                        arpB.setDocId(alArp.get(5).get(0));
-                        arpB.setDocName(alArp.get(6).get(0));
-                        arpB.setLocCode(alArp.get(7).get(0));
-                        arpB.setLocName(alArp.get(8).get(0));
-                        arpB.setComment(alArp.get(9).get(0));
-                        arpB.setRecHFCcode(alArp.get(10).get(0));
-                        arpB.setEpisodeDate(alArp.get(11).get(0));
-                        arpB.setEncounter_Date(query_alg_lhr_arp);
+                                query_alg_lhr_arp = "INSERT INTO lhr_procedure ("
+                                        + "pmi_no,"
+                                        + "hfc_cd,"
+                                        + "episode_date,"
+                                        + "encounter_date,"
+                                        + "procedure_cd,"
+                                        + "procedure_name,"
+                                        //+ "procedure_outcome,"
+                                        + "comment,"
+                                        + "doctor_id,"
+                                        + "doctor_name,"
+                                        + "national_id_no,"
+                                        + "person_id_no,"
+                                        + "person_status,"
+                                        + "centre_code) VALUES("
+                                        + "'"+arpB.getPMI_no()+"',"
+                                        + "'"+arpB.getRecHFCcode()+"',"
+                                        + "'"+arpB.getEpisodeDate()+"',"
+                                        + "'"+arpB.getEncounter_Date()+"',"
+                                        + "'"+arpB.getPrCode()+"',"
+                                        + "'"+arpB.getPrName()+"',"
+                                        //+ "'',"
+                                        + "'"+arpB.getComment()+"',"
+                                        + "'"+arpB.getDocId()+"',"
+                                        + "'"+arpB.getDocName()+"',"
+                                        + "'"+t.getNational_id_no()+"',"
+                                        + "'"+t.getPERSON_ID_NO()+"',"
+                                        + "'"+t.getPERSON_STATUS()+"',"
+                                        + "'"+t.getCentre_Code()+"'"
+                                        + ")";
 
-                        query_alg_lhr_arp = "";
+                                status_alg_lhr_arp = rc.setQuerySQL(Config.ipAddressServer, Config.portServer, query_alg_lhr_arp);
 
-                        status_alg_lhr_arp = rc.setQuerySQL(Config.ipAddressServer, Config.portServer, query_alg_lhr_arp);
+                                if (status_alg_lhr_arp == true) {
+                                    System.out.println("Done extract arp");
+                                } else {
+                                    System.out.println("False extract arp");
+                                    //System.out.println("query alg: "+query_alg_lhr_alg);
+                                }
+                                arpBr.add(arpB);
 
-                        if (status_alg_lhr_arp == true) {
-                            System.out.println("Done extract alg");
-                        } else {
-                            System.out.println("False extract alg");
-                            //System.out.println("query alg: "+query_alg_lhr_alg);
+                                //System.out.println(query_alg_lhr_alg);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                //System.out.println("Not a numbetr!");
+                            }
                         }
-                        arpBr.add(arpB);
-
-                        //System.out.println(query_alg_lhr_alg);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        //System.out.println("Not a numbetr!");
                     }
-                }
 
 //            update_ehr_central u = new update_ehr_central();
 //            u.update_status();
+                }
             }
         } catch (Exception e) {
             e.getStackTrace();
