277a1066d5885992769b7bd854937e85f68bb8a8
user-PC\Shammugam <shammugam94@gmail.com>
2017-07-09 01:42:11 +0800

    NIW IHIS CHART CIRCULATION VIEW HELPER CREATED

---
+++ src/java/NIW_helper/CirculationUtils.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NIW_helper;
+
+import dBConn.Conn;
+import java.util.ArrayList;
+import main.RMIConnector;
+
+/**
+ *
+ * @author Shammugam
+ */
+public class CirculationUtils {
+    
+}
